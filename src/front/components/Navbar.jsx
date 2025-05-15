367442a9676a000d538b08b49522abda905956cc
Antonino <LAZARONUNEZANTONINO@GMAIL.COM>
2025-05-15 08:57:26 +0000

    adding styles

--- src/front/components/Navbar.jsx
+++ src/front/components/Navbar.jsx
@@ -1,17 +1,13 @@
-import { Link } from "react-router-dom";
-
 export const Navbar = () => {
 
 	return (
-		<nav className="navbar navbar-light bg-light">
-			<div className="container">
-				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-				</Link>
-				<div className="ml-auto">
-					<Link to="/demo">
-						<button className="btn btn-primary">Check the Context in action</button>
-					</Link>
+		<nav class="navbar navbar-dark navbar-expand-lg navCss">
+			<div class="container">
+				<a class="navbar-brand" href="#">Gym Buddy</a>
+				<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+					<span class="navbar-toggler-icon"></span>
+				</button>
+				<div class="collapse navbar-collapse" id="navbarSupportedContent">
 				</div>
 			</div>
 		</nav>
--- src/front/pages/Home.jsx
+++ src/front/pages/Home.jsx
@@ -1,17 +1,17 @@
 import { Link } from "react-router-dom";
-import { useNavigate } from "react-router-dom";
+import image from "../assets/img/pruebaverde.png"
 
 export const Home = () => {
-	const navigate = useNavigate();
-
 	return (
-		<div className="bg-light min-h-screen flex items-center justify-center text-green-400 font-mono p-4">
-			<div className="bg-gray-950 p-8 rounded-2xl shadow-lg shadow-green-500 text-center max-w-xl p-4 rounded">
-				<h2 className="text-xl">
-					If you don`t have an account: <Link to={'/register'} className="underline hover:text-green-300">Register</Link><br />
-					If you already have an account:  <Link to={'/login'} className="underline hover:text-green-300">Login</Link><br />
-					After login, you can enter the private page: <Link to={'/private'} className="underline hover:text-green-300">Private Page</Link>.
+		<div className="back-img">
+			<img src={image} alt="gym" />
+			<div className="startBox rounded-4">
+				<h2 className="text-white mb-4 pb-4">
+					WELCOME TO <span className="text-white">GYM BUDDY</span>
 				</h2>
+				<Link to="/login" className="btn neon-btn w-50">
+					START
+				</Link>
 			</div>
 		</div>
 	);
